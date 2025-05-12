4fa0e582b49947d21851a4214f61d4da8e9687c6
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-05-12 19:54:18 +0600

    a

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000;
 
+app.use(cors())
+
 app.get('/', (req, res) => {
-    res.send('Hlw World')
-})
+    res.send('Hlw Developer')
+});
+const users = [
+    { id: 1, name: 'Sabana', email: 'sabana@gmail.com' },
+    { id: 2, name: 'Sabnoor', email: 'Sabnoor@gmail.com' },
+    { id: 3, name: 'Sabila', email: 'sabila@gmail.com' },
+];
+app.get('/users', (req, res) => {
+    res.send(users)
+} )
 
 app.listen(port, () => {
     console.log(`App is running on port ${port}`);
